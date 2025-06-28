2ba35da295aeb81a068ca1e01ea5b2935345a322
xugeboy <xugeboy@gmail.com>
2025-06-28 13:13:50 +0800

    modified:   app/[lang]/layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -157,6 +157,17 @@ export default async function RootLayout({
           title="Sitemap"
           href="/sitemap.xml"
         />
+        {/* Google Tag Manager */}
+        <Script id="GoogleTagManager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K925B3FL');
+        `}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Facebook Pixel script */}
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
@@ -180,6 +191,11 @@ export default async function RootLayout({
         ></script>
       </head>
       <body className={`${poppins.className} antialiased`}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K925B3FL"
+              height="0" width="0" style="display:none;visibility:hidden">
+          </iframe>
+        </noscript>
         {/* noscript fallback for users without JS */}
         <noscript>
           <img
@@ -203,7 +219,7 @@ export default async function RootLayout({
           </div>
         </CategoryProvider>
 
-        <Analytics />
+        {/* <Analytics /> */}
       </body>
     </html>
   );
