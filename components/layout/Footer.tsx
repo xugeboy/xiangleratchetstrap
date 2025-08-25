9648e8a66fb50ce5b085a6779ef8694d3d8978a3
xugeboy <xugeboy@gmail.com>
2025-08-25 09:34:52 +0800

    modified:   components/layout/Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -39,7 +39,7 @@ export default function Footer() {
       { nameKey: "support.downloadCatalog", href: "download-catalog" },
       { nameKey: "support.faq", href: "faq" },
       { nameKey: "support.businessSolutions", href: "business-solutions" },
-      { nameKey: "support.sitemap", href: "/sitemap.xml" },
+      { nameKey: "support.sitemap", href: "https://xiangleratchetstrap.com/sitemap.xml" },
     ],
     products: [
       {
