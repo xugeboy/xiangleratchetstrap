4552e3b0db8fd9dfd60a9bb7bc3148a5ec7171b2
xugeboy <xugeboy@gmail.com>
2026-02-07 13:25:30 +0800

    new file:   app/[lang]/(public)/fly-fish-retractable-ratchet-strap/innerPage.tsx 	modified:   app/[lang]/(public)/fly-fish-retractable-ratchet-strap/page.tsx

--- app/[lang]/(public)/fly-fish-retractable-ratchet-strap/page.tsx
+++ app/[lang]/(public)/fly-fish-retractable-ratchet-strap/page.tsx
@@ -1,123 +1,106 @@
-"use client";
+import { Metadata, ResolvingMetadata } from "next";
+import { defaultUrlPrefix, localePrefixMap } from "@/middleware";
+import { generateGeneralBreadcrumbs } from "@/utils/breadcrumbs";
+import { generateSchema, embedSchema } from "@/utils/schema";
+import { useLocale } from "next-intl";
+import { getLocale } from "next-intl/server";
+import InnerPage from "./innerPage";
 
-import { useEffect, useRef } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
-import { ReactLenis } from "lenis/react";
-import Image from 'next/image';
-import BlackSection from "./blackSection";
-import RedSection from "./redSection";
-import PinkSection from "./pinkSection";
-import BlueSection from "./blueSection";
-import OrangeSection from "./orangeSection";
-import GreySection from "./greySection";
-import GreenSection from "./greenSection";
+export async function generateMetadata(
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    const currentLocale = await getLocale();
 
-gsap.registerPlugin(ScrollTrigger);
+    const pageTitle = "Next-gen Automatic Retractable Ratchet Strap - Heavy Duty 1760 lbs. BS";
+    const description = "Next-gen automatic retractable ratchet strap with heavy-duty construction, 1760 lbs break strength, smooth operation, and fast retraction for cargo securing, transport, and industrial use.";
+    const pageSlug = "fly-fish-retractable-ratchet-strap";
+    const ogImageUrl = process.env.NEXT_PUBLIC_LOGO_URL;
+    const ogImageAlt = pageTitle;
+    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
+
+    let canonicalUrlPath;
+    if (currentLocale === defaultUrlPrefix || currentLocale === undefined) {
+        canonicalUrlPath = `/${pageSlug}`;
+    } else {
+        canonicalUrlPath = `/${currentLocale}/${pageSlug}`;
+    }
+    const canonicalUrl = `${siteUrl}${canonicalUrlPath}`;
+
+    const languagesAlternate: Record<string, string> = {};
+
+    for (const ietfTag in localePrefixMap) {
+        const targetUrlPrefix = localePrefixMap[ietfTag];
+        let pathForLang = "";
+        if (targetUrlPrefix === defaultUrlPrefix) {
+            pathForLang = `${siteUrl}/${pageSlug}`;
+        } else {
+            pathForLang = `${siteUrl}/${targetUrlPrefix}/${pageSlug}`;
+        }
+        languagesAlternate[ietfTag] = pathForLang;
+    }
+
+    return {
+        title: pageTitle,
+        alternates: {
+            canonical: canonicalUrl,
+            languages:
+                Object.keys(languagesAlternate).length > 0
+                    ? languagesAlternate
+                    : undefined,
+        },
+        description: description,
+        openGraph: {
+            title: pageTitle,
+            url: canonicalUrl,
+            images: [
+                {
+                    url: ogImageUrl,
+                    width: 1200,
+                    height: 630,
+                    alt: ogImageAlt,
+                },
+            ],
+        },
+        twitter: {
+            title: pageTitle,
+        },
+    };
+}
+
 
 export default function Home() {
-    const lenisRef = useRef(null);
-    const containerRef = useRef(null);
+    const locale = useLocale();
 
-    useEffect(() => {
-        function update(time) {
-            lenisRef.current?.lenis?.raf(time * 1000);
-        }
-
-        lenisRef.current?.lenis?.on("scroll", ScrollTrigger.update);
-        gsap.ticker.add(update);
-        gsap.ticker.lagSmoothing(0);
-
-        return () => gsap.ticker.remove(update);
-    }, []);
-
-    useGSAP(
-        () => {
-            const sections = document.querySelectorAll("section");
-
-            sections.forEach((section, index) => {
-                const container = section.querySelector(".containerx");
-
-                gsap.to(container, {
-                    rotation: 0,
-                    ease: "none",
-                    scrollTrigger: {
-                        trigger: section,
-                        start: "top bottom",
-                        end: "top 20%",
-                        scrub: true,
-                    },
-                });
-
-                if (index === sections.length - 2) return;
-
-                ScrollTrigger.create({
-                    trigger: section,
-                    start: "bottom bottom",
-                    end: "bottom top",
-                    pin: true,
-                    pinSpacing: false,
-                });
-            });
-        },
-        { scope: containerRef },
+    const breadcrumbItems = generateGeneralBreadcrumbs(
+        "fly fish retractable ratchet strap",
+        "fly-fish-retractable-ratchet-strap",
+        locale
     );
-
+    const websiteSchema = generateSchema({
+        type: "WebSite",
+        lang: locale,
+    });
+    const organizationSchema = generateSchema({
+        type: "Organization",
+        lang: locale,
+    });
+    const breadcrumbSchema = generateSchema({
+        type: "BreadcrumbList",
+        breadcrumbItems,
+    });
+    const schemaMetadataJson = embedSchema(
+        [websiteSchema, organizationSchema, breadcrumbSchema].filter(Boolean)
+    );
 
     return (
         <>
-            <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
-            <main ref={containerRef} className="w-full">
-                {/* Section 1: Black */}
-                <section className="relative w-full h-[100svh] min-h-[100svh] overflow-hidden z-10 two">
-                    <div className="containerx relative w-full h-full p-8 flex flex-col lg:flex-row rotate-[30deg] origin-bottom-left will-change-transform bg-[#000000]">
-                        <BlackSection/>
-                    </div>
-                </section>
-
-                {/* Section 2: Red */}
-                <section className="relative w-full h-[100svh] min-h-[100svh] overflow-hidden z-10 two">
-                    <div className="containerx relative w-full h-full p-8 flex flex-col lg:flex-row rotate-[30deg] origin-bottom-left will-change-transform bg-[#ff0004]">
-                        <RedSection/>
-                    </div>
-                </section>
-
-                {/* Section 3: PINK */}
-                <section className="relative w-full h-[100svh] min-h-[100svh] overflow-hidden z-10 three">
-                    <div className="containerx relative w-full h-full p-8 flex flex-col lg:flex-row rotate-[30deg] origin-bottom-left will-change-transform bg-[#ff8bdc]">
-                        <PinkSection/>
-                    </div>
-                </section>
-
-                {/* Section 4: BLUE */}
-                <section className="relative w-full h-[125svh] lg:h-[150svh] min-h-[100svh] overflow-hidden z-10 four">
-                    <div className="containerx relative w-full h-full p-8 flex flex-col justify-center items-center text-center gap-4 rotate-[30deg] origin-bottom-left will-change-transform bg-[#009dff]">
-                        <BlueSection></BlueSection>
-                    </div>
-                </section>
-
-                {/* Section 5: GREEN */}
-                <section className="relative w-full h-[100svh] min-h-[100svh] overflow-hidden z-10 five">
-                    <div className="containerx relative w-full h-full p-8 flex flex-col lg:flex-row rotate-[30deg] origin-bottom-left will-change-transform bg-[#3a5a40]">
-                        <GreenSection></GreenSection>
-                    </div>
-                </section>
-
-                {/* Section 6: ORANGE */}
-                <section className="relative w-full h-[100svh] min-h-[100svh] overflow-hidden z-10 six">
-                    <div className="containerx relative w-full h-full p-8 flex flex-col lg:flex-row rotate-[30deg] origin-bottom-left will-change-transform bg-[#ffbf00]">
-                        <OrangeSection></OrangeSection>
-                    </div>
-                </section>
-
-                {/* Section 7: GREY */}
-                <section className="relative w-full h-[100svh] min-h-[100svh] overflow-hidden z-10 one">
-                    <div className="containerx relative w-full h-full p-8 flex flex-col lg:flex-row rotate-[30deg] origin-bottom-left will-change-transform bg-[#c2c1c2] ">
-                        <GreySection></GreySection>
-                    </div>
-                </section>
-            </main>
+            <section>
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{ __html: schemaMetadataJson }}
+                />
+            </section>
+            <InnerPage></InnerPage>
         </>
     );
 }
