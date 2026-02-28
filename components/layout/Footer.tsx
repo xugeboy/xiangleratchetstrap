3dead72f656250dcc3b6a4e2504c96a6cf3dca3b
xugeboy <xugeboy@gmail.com>
2026-02-28 14:55:39 +0800

    modified:   components/layout/Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -426,7 +426,7 @@ export default function Footer() {
             ))}
           </div>
           <p className="mt-8 text-sm/6 text-white md:order-1 md:mt-0">
-            &copy; 2025 Xiangle Ratchet Strap. All rights reserved.
+            &copy; 2026 Xiangle Ratchet Strap. All rights reserved.
           </p>
         </div>
       </div>
